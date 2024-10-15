344eb980eff182429f78fd9f9d9588377151c895
adrien-caltagirone <adrien.caltagirone@edu.esiee.fr>
2024-10-15 06:44:02 +0000

    done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+''' Module '''
 #### Imports et définition des variables globales
-import random
+import csv
 
 FILENAME = "listes.csv"
 
@@ -14,40 +15,54 @@ def read_data(filename):
     Returns:
         list: le contenu du fichier (1 list par ligne)
     """
-    l = []
-    return l
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        r = csv.reader(f, delimiter=';')
+        l = list(r)
+    return [list(map(int, x)) for x in l]
 
 def get_list_k(data, k):
-    l = []
-    return l
+    ''' Function '''
+    return data[k]
 
 def get_first(l):
-    return None
+    ''' Function '''
+    return int(l[0])
 
 def get_last(l):
-    return None
+    ''' Function '''
+    return l[-1]
 
 def get_max(l):
-    return None
+    ''' Function '''
+    return max(l)
 
 def get_min(l):
-    return None
+    ''' Function '''
+    return min(l)
 
 def get_sum(l):
-    return None
+    ''' Function '''
+    return sum(int(i) for i in l)
 
 
 #### Fonction principale
 
 
 def main():
-    pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
+    ''' Function '''
+    # pass
+    data = read_data(FILENAME)
+    for i, l in enumerate(data):
+        print(i, l)
+    k = 37
+    print(k, get_list_k(data, 37))
 
+    print(get_first(get_list_k(data, 37)))
+    print(get_last(get_list_k(data, 37)))
+    print(get_max(get_list_k(data, 37)))
+    print(get_min(get_list_k(data, 37)))
+    print(get_sum(get_list_k(data, 37)))
 
 if __name__ == "__main__":
     main()
